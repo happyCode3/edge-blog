e8f9953efbab6e01325557cda834efd85158f67a
coderblower <saifulrmail@gmail.com>
2020-01-26 03:58:33 +0600

    add poast clear

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const Sequelize = require('sequelize');
-const sequelize = require('./models');
 const Post = require('./models').Post;
 const {config, engine} = require('express-edge')
 const bodyParser = require('body-parser')
@@ -29,39 +28,115 @@ app.set('views', `${__dirname}/views`)
 app.use(express.static('public'))
 
 
-
 app.use(bodyParser.urlencoded({
    extended: true
  }))
  app.use(bodyParser.json())
 
 
-
-app.get('/',(req,res)=>{
+app.get('/',   (req,res)=>{
    let data;
-   Post.findAll()
-            .then(users => {
-               data = JSON.parse(JSON.stringify(users, null, 4));
-               console.log(data)
-               res.render('index', {data})
-         })
-   
+   Post.findAll({
+      order: [
+         ["updatedAt", 'DESC'],
+         ['id', 'DESC']
+     ]
+   }).then(users => {
+     data = JSON.parse(JSON.stringify(users, null, 4));
+     res.render('index', {data})
+    });
+      
 })
-app.get('/update',(req,res)=>{
-    res.render('update')
+
+function getSingleData(req,res,next){
+   let {id} = req.params
+   var data
+      Post.findAll({
+         where: {
+           id
+         }
+       }).then(users => {
+            
+            data = JSON.parse(JSON.stringify(users, null, 4));
+           
+            req.data = data
+           
+         if(data.length<1){
+            res.send('opps you serach incorrectly')
+         }
+         next()
+   
+        });
+        
+   }
+
+
+app.get('/updatePost/:id', getSingleData, (req,res)=>{
+
+   let data = req.data
+   
+     res.render('update', {data})
+     console.log('data',data)
  })
 
- app.get('/post', (req,res)=>{
+
+ app.get('/create-post', (req,res)=>{
 
     res.render('post')
  })
 
- app.post('/post', upload.single('image'),(req,res)=>{
+ app.get('/delete/:id', (req,res)=>{
+
+   Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+   res.redirect('/')
+ })
+
+ app.post('/updatePost/:id', upload.single('image'), (req,res)=>{
+    let {title, body } = req.body
+
+    if(!req.image){
+
+       Post.update({
+          title, body
+         }, {
+            where: {
+               id: req.params.id
+            }
+         });
+      } else{
+         Post.update({
+            title, body, image:req.image
+           }, {
+              where: {
+                 id: req.params.id
+              }
+           });
+      }
+  
+      res.redirect('/')
+  
+})
+
+ app.post('/create-post', upload.single('image'),(req,res)=>{
     let {title,body} = req.body;
       Post.create({
         title, body, image:req.image
       })
-   //  res.render('index')
-   res.render('index')
+      let data;
+      Post.findAll({
+         order: [
+            ["updatedAt", 'DESC'],
+            ['id', 'DESC']
+        ]
+      }).then(users => {
+        data = JSON.parse(JSON.stringify(users, null, 4));
+        res.redirect('/')
+       });
+       
  })
 app.listen(3000,()=>console.log('port open at 3000'))
